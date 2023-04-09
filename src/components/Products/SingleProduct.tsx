a31688dad799a451092d52aa2e1c53457bf3f66a
umut.ketenci <umut.ketenci@smartict.com.tr>
2023-04-09 22:32:18 +0300

    changd product bg-color to turn green when selcted

--- src/components/Products/SingleProduct.tsx
+++ src/components/Products/SingleProduct.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { Product } from "../../store/store";
+import { Product, RootState, VendingMachineState } from "../../store/store";
+import { useSelector } from "react-redux";
 
 interface ChildProp {
   product: Product;
 }
 const SingleProduct = (props: ChildProp) => {
+  const vendingMachineState: VendingMachineState = useSelector(
+    (state: RootState) => state.vendingMachineState
+  );
   return (
-    <div className="single-product">
+    <div
+      className="single-product"
+      style={{
+        backgroundColor:
+          vendingMachineState.selectedProduct?.id === props.product.id
+            ? "green"
+            : "transparent",
+      }}
+    >
       <p>{props.product.name || "Empty"}</p>
       <p>{props.product.quantity} quantity</p>
       <p>{props.product.price} Unit </p>
